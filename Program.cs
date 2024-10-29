c229c2175b58e697542422710206ff8cfa809f77
Kab-31-14 <Kab-31-14@KAB-31-14-PC>
2024-10-29 12:17:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1.1
+            //Console.Write("Введите слово: ");
+            //string slovo = Console.ReadLine();
+            //Console.Write("Введите n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //while (count <= n)
+            //{
+            //    Console.WriteLine(slovo);
+            //    count++;
+            //}
+
+            //1.2
+            //Console.Write("Введите слово: ");
+            //string slovo = Console.ReadLine();
+            //Console.Write("Введите n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //do
+            //{
+            //    Console.WriteLine(slovo);
+            //    count++;
+            //}
+            //while (count <= n); 
+
+            //Task 1.3
+            //Console.Write("Введите слово: ");
+            //string slovo = Console.ReadLine();
+            //Console.Write("Введите n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int count = 1; count <= n; count++)
+            //{
+            //    Console.WriteLine(slovo);
+            //}
+
+            //Task 2.1
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Task 2.2
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i <= 10);
+
+            //Task 2.3
+            //for (int i = 1 ;i<=10 ;i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Task 3.1
+            //int i = 5;
+            //while(i >= 1)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //Task 3.2
+            //int i = 5;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+            //while (i >= 1);
+
+            //Task 3.3
+            //for (int i = 5;  i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Task 4.1
+            //Console.Write("Введите n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //while (count <= n)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //} 
+
+
+
+
+
+            //Practicum #8
+            //Task 1
+            //int[]omas = new int[8];
+            //Console.WriteLine("Заполнение одномерного массива: ");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Вывод одномерного массива:");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"omas[{i}] = {omas[i]}");
+            //}
+
+            //Task 2
+            //int[] omas = new int[12] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            //Console.WriteLine("Вывод одномерного массива:");
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.Write($"omas[{i}] = {omas[i]}");
+            //}
+
+            //Task 3
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //for(int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(163,190);
+
+            //}
+            //Console.WriteLine("Вывод одномерного массива:");
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.Write($"omas[{i}] = {omas[i]}");
+            //}
+
+            //int[] omas = new int[31];
+            //Random rnd = new Random();
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(5, 25);
+
+            //}
+            //Console.WriteLine("Вывод одномерного массива:");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.Write($"omas[{i}] = {omas[i]}");
+            //}
+            //int summ = 0;
+            //for(int i = 0;i < omas.Length; i++)
+            //{
+            //    //summ = summ + omas[i];
+            //    summ += omas[i];
+
+            //} 
+            //Console.WriteLine($" Средняя температруа равна {summ/omas.Length}");
+
+
+            Console.Read();
+        }
+    }
+}
